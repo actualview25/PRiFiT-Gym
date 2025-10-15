035dcac3afad3becd374a84b860dbd77296211d7
actualview25 <actual.view25@gmail.com>
2025-10-16 01:53:34 +0300

    Change fov value from 1.5708 to 1.0 in data.js

--- data.js
+++ data.js
@@ -30,7 +30,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -68,7 +68,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -118,7 +118,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -162,7 +162,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -200,7 +200,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -244,7 +244,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -292,7 +292,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.0
       },
       "linkHotspots": [
         {
@@ -336,10 +336,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "yaw": 0.08119990399638866,
         "pitch": 0.2180647351189542,
-        "fov": 1.5707963267948966
-        "zoom": 0,
-        "minZoom": 0,
-        "minZoom": 2,
+        "fov": 1.0
       },
       "linkHotspots": [
         {
